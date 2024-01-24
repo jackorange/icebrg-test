32135ce56c674f7c0eddf89215dfacdfe731d037
Igor Hardzeyeu <ihar.hardzeyeu@gmail.com>
2024-01-24 14:26:14 +0300

    Add constants file

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const API_DOMAIN = 'https://icebrg.mehanik.me/api/';
+export const API_LOGIN = `${API_DOMAIN}login`;
+export const API_LOGIN_REFRESH = `${API_LOGIN}/refresh`;
+export const API_SEARCH = `${API_DOMAIN}search`;
+
+export const LOGIN_ROUTE = '/login';
+export const SEARCH_ROUTE = '/';
