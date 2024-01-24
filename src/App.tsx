b3fd1d31f3c97ab03e3db1a548519d134fd11af8
Igor Hardzeyeu <ihar.hardzeyeu@gmail.com>
2024-01-24 14:27:07 +0300

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
+import { ReactNode } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Search from './pages/Search';
 import Login from './pages/Login';
+import { ApiProvider } from './hooks/useApi';
+import './App.css';
+import { LOGIN_ROUTE, SEARCH_ROUTE } from './constants';
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
 
 const App = () => {
+  const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+    if (!localStorage.getItem('access_token')) {
+      return <Navigate to="/login" replace />;
+    }
+    return <>{children}</>;
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Search />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <ApiProvider>
+          <Routes>
+            <Route
+              path={SEARCH_ROUTE}
+              element={
+                <ProtectedRoute>
+                  <Search />
+                </ProtectedRoute>
+              }
+            />
+            <Route path={LOGIN_ROUTE} element={<Login />} />
+          </Routes>
+        </ApiProvider>
       </BrowserRouter>
     </div>
   );
